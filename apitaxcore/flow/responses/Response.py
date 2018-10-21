1e9ce0516c05faebd42b53755e1e6c95bc87246e
Shawn Clake <shawn.clake@gmail.com>
2018-10-21 16:30:59 -0600

    Smushed responses bug fix

--- apitaxcore/flow/responses/Response.py
+++ apitaxcore/flow/responses/Response.py
@@ -4,23 +4,23 @@ from apitaxcore.flow.responses.LowLevelResponse import LowLevelResponse
 
 
 class Response(LowLevelResponse):
-    def __init__(self, status=None, headers=HeaderBuilder(), body=BodyBuilder()):
+    def __init__(self, status=None, headers=None, body=None):
         super().__init__()
         self.status = status
-        self.headers = headers
-        self.body = body
+        self.headers: HeaderBuilder = headers if headers is not None else HeaderBuilder()
+        self.body: BodyBuilder = body if body is not None else BodyBuilder()
 
     def getResponseHeaders(self):
-        if (len(self.headers.get()) > 0):
+        if len(self.headers.get()) > 0:
             return self.headers.get()
         return super().getResponseHeaders()
 
     def getResponseBody(self):
-        if (len(self.body.get()) > 0):
+        if len(self.body.get()) > 0:
             return self.body.get()
         return super().getResponseBody()
 
     def getResponseStatusCode(self):
-        if (self.status):
+        if self.status:
             return self.status
         return super().getResponseStatusCode()
